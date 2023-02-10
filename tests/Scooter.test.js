e1ded863c53249f1c1784e2ce32512c9c9bfbecb
ChrisByrn3 <chris.byrn3@gmail.com>
2023-02-10 12:02:01 +0000

    Scooter property tests defined

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -2,14 +2,36 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
+describe('scooter properties tests', () => {
+  
+  const scooter = new Scooter("Mere Lane");
+
+  test('instance properties', () => {
+    expect(typeof scooter.user).toBe("string");
+    expect(scooter).toHaveProperty("serial");
+    expect(scooter).toHaveProperty("charge");
+    expect(typeof scooter.isBroken).toBe("Boolean")
+    expect(scooter.station).toBe("Mere Lane")
   }
 )
+
+test('check nextSerial is incrementing', () => {
+ 
+  const scooter2 = new Scooter("Ash Road");
+
+  expect(scooter2.serial).toBe(scooter.serial +1);
+})
 })
 
+
+
+// Each `Scooter` should have the following methods
+// - `rent()`
+//     - If the `Scooter` is charged above 20% and not broken, remove it from its station, check it out to user. 
+//     - Otherwise, throw an error scooter needs to charge or scooter needs repair.
+
+// - `dock(station)`
+//     - Return the scooter to the station. Be sure to clear out the user, so they don’t get charged unfairly!
 //Method tests
 describe('scooter methods', () => {
   // tests here!
